9362752ab2d50a457d28df87430dca28e66398bc
Martin <madetho@live.de>
2019-04-02 19:26:19 +0200

    polak drop new commit 1 tap uff ya

--- index.js
+++ index.js
@@ -3,126 +3,167 @@ const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = process.env.TELEGRAM_TOKEN || '565495861:AAFD8f1OFJ1OsaN6nGhqP-_pwjIA3We8HKg';
 const port = process.env.PORT || 8443;
 const url = process.env.APP_URL || 'https://livechamslave.herokuapp.com:443';
-const bot = new TelegramBot(TOKEN, {webHook: {port: port}});
 
-//const bot = new TelegramBot(TOKEN, {polling: true});
+//Dev
+const bot = new TelegramBot(TOKEN, {polling: true});
 
-bot.setWebHook(`${url}/bot${TOKEN}`);
+//Prod
+//const bot = new TelegramBot(TOKEN, {webHook: {port: port}});
+//bot.setWebHook(`${url}/bot${TOKEN}`);
 
 let isWatching = false;
-bot.onText(/\/live (\d{7})/, (msg, match) => {
-    let roundFeed = "";
-    let matchId = match[1];
-
+let liveMatch;
+bot.onText(/\/live (\d{7})/, async (msg, match) => {
     if (!isWatching) {
+        let matchId = match[1];
         //First try to get the match
-        HLTV.getMatch({ id: matchId }).then(match => {
-            //Check if match ist valid
-            if (match && match.live) {
-                sendMessage(msg.chat.id, "Started watching: " + match.team1.name + " vs. " + match.team2.name);
+        let fullMatch = await HLTV.getMatch({ id: matchId });
+        //Check if match is valid and live
+        if (fullMatch && fullMatch.live) {
+            let roundData = {};
+            let pinnedMessage = await bot.sendMessage(msg.chat.id, "Fetching...");
 
-                let roundFeed = "";
-                //Connect to livefeed
-                let livematch = HLTV.connectToScorebot({
-                    id: matchId, onScoreboardUpdate: (data) => {
+            updateMessage(fullMatch, null, msg.chat.id, pinnedMessage.message_id);
 
-                    }, onLogUpdate: (data) => {
-                        if (isWatching) {
-                            switch (Object.keys(data.log[0])[0]) {
-                                case "Kill": {
-                                    roundFeed += getKillFeed(data.log[0].Kill) + "\n";
-                                    break;
-                                }
-                                case "RoundStart": {
-                                    roundFeed = "";
-                                    break;
-                                }
-                                case "BombPlanted": {
-                                    roundFeed += getBombPlantFeed(data.log[0].BombPlanted) + "\n";
-                                    break;
-                                }
-                                case "BombDefused": {
-                                    roundFeed += getBombDefusedFeed(data.log[0].BombDefused) + "\n";
-                                    break;
-                                }
-                                case "RoundEnd": {
-                                    roundFeed += getRoundEndFeed(data.log[0].RoundEnd);
-                                    sendMessage(msg.chat.id, roundFeed);
-                                    break;
-                                }
-                            }
-                        } else {
-                            livematch = null;
-                        }
-                    }, onConnect: () => {
-                        isWatching = true;
-                        sendMessage(msg.chat.id, "We are live bois!");
-                    }, onDisconnect: () => {
-                        isWatching = false;
-                        sendMessage(msg.chat.id, "We disconnected!");
-                        throw 'cancel';
+            //Connect to livefeed
+            liveMatch = HLTV.connectToScorebot({
+                id: matchId, onScoreboardUpdate: (scoreboard) => {
+                    if(roundData.ctTeamName !== scoreboard.ctTeamName) {
+                        roundData.ctTeamName = scoreboard.ctTeamName;
+                        roundData.tTeamName = scoreboard.terroristTeamName;
                     }
-                }).catch(err => {
-                    sendMessage(msg.chat.id, "Some error happend: Canceled watching match!");
-                });
-            } else if (!match) {
-                sendMessage(msg.chat.id, "Match is invalid!");
-            } else if (!match.live) {
-                sendMessage(msg.chat.id, "Match is not live yet!");
-            }
-        })
+                    roundData.currentMapName = scoreboard.mapName;
+                }, onLogUpdate: (data) => {
+                    if (isWatching) {
+                        switch (Object.keys(data.log[0])[0]) {
+                            case "Kill": {
+                                roundData.lastStatus = getKillFeed(data.log[0].Kill);
+                                updateMessage(fullMatch, roundData, msg.chat.id, pinnedMessage.message_id);
+                                break;
+                            }
+                            case "RoundStart": {
+                                roundData.lastStatus = "Round started..";
+                                updateMessage(fullMatch, roundData, msg.chat.id, pinnedMessage.message_id);
+                                break;
+                            }
+                            case "BombPlanted": {
+                                roundData.lastStatus = getBombPlantFeed(data.log[0].BombPlanted);
+                                updateMessage(fullMatch, roundData, msg.chat.id, pinnedMessage.message_id);
+                                break;
+                            }
+                            case "BombDefused": {
+                                roundData.lastStatus = getBombDefusedFeed(data.log[0].BombDefused);
+                                updateMessage(fullMatch, roundData, msg.chat.id, pinnedMessage.message_id);
+                                break;
+                            }
+                            case "RoundEnd": {
+                                roundData.roundEnd = data.log[0].RoundEnd;
+                                roundData.lastStatus += " | " + roundData.roundEnd.winner + " won - " + roundData.roundEnd.winType;
+                                updateMessage(fullMatch, roundData, msg.chat.id, pinnedMessage.message_id);
+                                break;
+                            }
+                        }
+                    } else {
+                        liveMatch = null;
+                    }
+                }, onConnect: () => {
+                    isWatching = true;
+                    sendMessage(msg.chat.id, "Connected to match!");
+                }, onDisconnect: () => {
+                    isWatching = false;
+                    sendMessage(msg.chat.id, "Error: Disconnected to match!");
+                    throw 'cancel';
+                }
+            }).catch(err => {
+                sendMessage(msg.chat.id, "Some error happend: Canceled watching match!");
+            });
+        } else if (!fullMatch) {
+            sendMessage(msg.chat.id, "Match is invalid!");
+        } else if (!fullMatch.live) {
+            sendMessage(msg.chat.id, "Match is not live yet!");
+        }
     } else {
-        sendMessage(msg.chat.id, "Allready watching a match!");
+        sendMessage(msg.chat.id, "Allready watching a match. Please use /stop first to watch a new match!");
     }
 });
 
-bot.onText(/\/stop/, (msg, match) => {
+bot.onText(/\/stop/, (msg) => {
     if(isWatching) {
         isWatching = false;
-        sendMessage(msg.chat.id, "Stopped watching match");
+        liveMatch = null;
+        sendMessage(msg.chat.id, "Stopped watching match.");
     }
 });
 
-getKillFeed = (killData) => {
+function getKillFeed(killData) {
     let killFeed = "";
     killFeed += killData.killerNick;
-    //killFeed += "(" + getSideSmall(killData.killerSide) + ")";
     killFeed += " " + (killData.headShot ? "\u{1F480}" : "\u{1F44A}") + " ";
     killFeed += killData.victimNick;
-    //killFeed += "(" + getSideSmall(killData.victimSide) + ")";
-    //killFeed += " with";
-    //killFeed += " " + killData.weapon;
-    //killFeed += " " + (killData.headShot ? "(hs)" : "");
     return killFeed
 }
 
-getBombPlantFeed = (bombPlantData) => {
+function getBombPlantFeed(bombPlantData) {
     let bombPlantFeed = "";
     bombPlantFeed += bombPlantData.playerName;
     bombPlantFeed += " planted \u{1F4A3} ";
-    //bombPlantFeed += "(" + bombPlantData.tPlayers + "on" + bombPlantData.ctPlayers + ")";
+    bombPlantFeed += "(" + bombPlantData.tPlayers + "on" + bombPlantData.ctPlayers + ")";
     return bombPlantFeed
 }
 
-getBombDefusedFeed = (bombDefusedData) => {
+function getBombDefusedFeed(bombDefusedData) {
     let bombDefusedFeed = bombDefusedData.playerNick;
     bombDefusedFeed += " defused \u{1F527}";
     return bombDefusedFeed
 }
 
-getRoundEndFeed = (roundEndtData) => {
-    let roundEndFeed = ""
-    roundEndFeed += roundEndtData.winner + " won";
-    //roundEndFeed += " - " + roundEndtData.winType;
-    roundEndFeed += "\n";
-    roundEndFeed += "\u{1F46E} " + roundEndtData.counterTerroristScore + ":" + roundEndtData.terroristScore + " \u{1F473}";
+function getRoundEndFeed(roundData, fullMatch) {
+    let roundEndFeed = fullMatch.team1.name + " vs. " + fullMatch.team2.name + " - Waiting for round end..";
+    if(roundData.roundEnd) {
+        roundEndFeed = roundData.ctTeamName + " (CT) " + roundData.roundEnd.counterTerroristScore + " - " + roundData.roundEnd.terroristScore + " (T) " + roundData.tTeamName;
+    }
+    roundEndFeed += " | " + roundData.currentMapName + (roundData.lastStatus ? ( " | " + roundData.lastStatus) : "");
     return roundEndFeed
 }
 
-getSideSmall = (side) => {
+function getSideSmall(side) {
     return side === "TERRORIST" ? "T" : side;
 }
 
-sendMessage = (chatid, roundFeed) => {
-    bot.sendMessage(chatid, roundFeed);
+function sendMessage(chatId, roundFeed) {
+    bot.sendMessage(chatId, roundFeed);
+}
+
+function editMessage(chatId, messageId, roundFeed) {
+    bot.editMessageText(roundFeed, {chat_id: chatId, message_id: messageId});
+}
+
+function getStaticMatchDataString(fullMatch) {
+    let staticFullMatchString = "\n\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n";
+    staticFullMatchString += "Format: " + fullMatch.format + "\n";
+    staticFullMatchString += "\nMaps: \n";
+    fullMatch.maps.forEach(map => {
+        staticFullMatchString += map.name + (map.result ? (" - " + map.result) : "") + "\n";
+    });
+    staticFullMatchString += "\nStreams: \n";
+    fullMatch.streams.forEach(stream => {
+        if("HLTV Live" !== stream.name)
+            staticFullMatchString += stream.name + " [" + stream.viewers + "] - " + stream.link + "\n";
+    });
+    return staticFullMatchString;
+}
+
+function updateMessage(fullMatch, roundData, chatId, messageId) {
+    let roundMessage = "";
+    if(fullMatch) {
+        if(!roundData) {
+            roundMessage += fullMatch.team1.name + " vs. " + fullMatch.team2.name + " - Waiting for round end..";
+            roundMessage += getStaticMatchDataString(fullMatch);
+        } else {
+            roundMessage += getRoundEndFeed(roundData, fullMatch);
+            roundMessage += getStaticMatchDataString(fullMatch);
+        }
+    }
+
+    editMessage(chatId, messageId, roundMessage);
 }
